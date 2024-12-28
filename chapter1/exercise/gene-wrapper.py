285c950217d92b55c521d67da6d6d3eb568125af
marceloserpa <jdev.marcelo@gmail.com>
2024-12-28 18:01:37 -0300

    re-implementing CompressedGene using BitStringWrapper

--- chapter1/exercise/gene-wrapper.py
+++ chapter1/exercise/gene-wrapper.py
@@ -5,11 +5,16 @@ class BitStringWrapper:
 
     def __init__(self) -> None:
         self._value: int = 1 # it avoid to print a extra character
-        self._shift: int = 2 # in this moment is FIXED but it should be dynamic
 
         # map all the possibilities
         self._letter_to_binary: Dict[str, int] = {"A": 0b00, "C": 0b01, "G": 0b10, "T": 0b11}
         self._binary_to_letter: Dict[int, str] = {0b00: "A", 0b01:"C", 0b10: "G", 0b11: "T"}
+
+        possible_elements: int = len(self._binary_to_letter)
+        possible_elements_round: int = possible_elements - (possible_elements % 2)
+
+        self._shift: int = int(possible_elements_round / 2)
+        print(self._shift)
 
     def append(self, letter: str) -> None:
         self._value <<= self._shift
@@ -35,27 +40,41 @@ class BitStringWrapper:
 
         return result  
 
+    def get_size(self) -> int:
+        return getsizeof(self._value)
+
+
+class CompressedGene:
+    
+    def __init__(self, gene: str) -> None:
+        self._bit_string: BitStringWrapper = BitStringWrapper()
+        self._compress(gene)
+
+    
+    def _compress(self, gene: str) -> None:
+        for nucleotide in gene.upper():
+            self._bit_string.append(nucleotide)
+
+    def decompress(self) -> str:
+        bit_string_interable = iter(self._bit_string)
+        gene = ""
+        for nucleotide in bit_string_interable:
+            gene += nucleotide
+        return gene
+
+    def get_size(self) -> int:
+        return self._bit_string.get_size()
+
+
 
 if __name__ == "__main__":
     
     from sys import getsizeof
-    gene:str = "TAGGGATTAACCGTTATATATATATAGC" * 100
+    original:str = "TAGGGATTAACCGTTATATATATATAGC" * 100
 
-    bit = BitStringWrapper()
-    for nucleotide in gene.upper():
-        bit.append(nucleotide)
+    print("original value = {}".format(original))
+    print("original is {} bytes".format(getsizeof(original)))
 
-    print("*****")
-
-    print(bit.get_binary())
-
-
-
-    myiter = iter(bit)
-    decompressed = ""
-    for x in myiter:
-        decompressed += x
-
-    print(gene)
-    print("---")
-    print(decompressed)
+    compressed : CompressedGene = CompressedGene(original)
+    print("compressed value = {}".format(compressed.decompress()))
+    print("compressed is {} bytes".format(compressed.get_size()))
