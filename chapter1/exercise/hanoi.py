cb870507abc7c59799af4a88852e02ac6ff7114a
marceloserpa <jdev.marcelo@gmail.com>
2024-12-29 15:25:47 -0300

    hanoi improving the algorithm

--- chapter1/exercise/hanoi.py
+++ chapter1/exercise/hanoi.py
@@ -40,21 +40,37 @@ class HanoiTowerSolver:
         self._towers.append(Stack("A"))
         self._towers.append(Stack("B"))
         self._towers.append(Stack("C"))
+        self._moves: int = 0
 
-    def move_disk(self, source: Stack[int], destination: Stack[int], auxiliar: Stack[int], n: int) -> None:
+    def move_disk(self, source: Stack[int], destination: Stack[int], auxiliar_towers: List[Stack[int]], n: int) -> None:
+        print(f'#move = {self._moves} source = {source} destination = {destination} aux-towers = {auxiliar_towers}')
+        
+        
+        self._moves += 1
         if n == 1:
             destination.push(source.pop())
-        else:
-            self.move_disk(source, auxiliar, destination, n - 1)
-            self.move_disk(source, destination, auxiliar, 1)
-            self.move_disk(auxiliar, destination , source, n -1 )
+
+        if n != 1 and len(auxiliar_towers) == 1:
+            self.move_disk(source, auxiliar_towers[0], [destination], n - 1)
+            
+            self.move_disk(source, destination, auxiliar_towers, 1)
+            
+            self.move_disk(auxiliar_towers[0], destination , [source], n -1 )
+
 
     def solve(self) -> None:
         for i in range(1, self._disks + 1):
             self._towers[0].push(i)
-        self.move_disk(self._towers[0], self._towers[2], self._towers[1], self._disks)        
+
+        source_tower: Stack[int] = self._towers[0]
+        dest_tower: Stack[int] = self._towers[-1:][0] # extract a list with only the last item and get the first element from the new list
+        aux_towers: List[Stack[int]] = self._towers[1:-1] # create new list without the first and the last element
+
+        self.move_disk(source_tower, dest_tower, aux_towers, self._disks)        
 
     def print(self) -> None:
+        print(f'total moves = {self._moves}')
+
         for tower in self._towers:
             tower.print()
 
@@ -62,7 +78,6 @@ class HanoiTowerSolver:
 
 if __name__ == "__main__":
 
-    num_discs :int = 3
     hanoiTowerSolver: HanoiTowerSolver = HanoiTowerSolver(3)
     hanoiTowerSolver.solve()
 
