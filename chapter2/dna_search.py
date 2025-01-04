a5bf79ed01f81c1155bc117a0fddc61cfd5fc79a
marceloserpa <jdev.marcelo@gmail.com>
2025-01-04 14:54:48 -0300

    basic dna representation

---
+++ chapter2/dna_search.py
@@ -0,0 +1,23 @@
+from enum import IntEnum
+from typing import Tuple, List
+
+Nucleotide: IntEnum = IntEnum("Nucleotide", ('A', 'C', 'G', 'T'))
+Codon = Tuple[Nucleotide, Nucleotide, Nucleotide]
+Gene = List[Codon]
+
+gene_str: str = "ACGTGGCTCTCTAACGTACGTACGTACGGGTTTATATATATCCCTAGGATCTCCCTTT"
+
+def string_to_gene(s: str) -> Gene:
+    gene: Gene = []
+    for i in range(0, len(s), 3):
+        if(i + 2) >=len(s):
+            return gene
+        
+        codon: Codon = (Nucleotide[s[i]], Nucleotide[s[i + 1]], Nucleotide[s[i + 2]])
+        gene.append(codon)
+
+    return gene
+
+
+my_gene: Gene = string_to_gene(gene_str)
+print(my_gene)
