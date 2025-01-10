2f1c2bf66be7e4fe89f4dd7f83e8ad513c96d41a
marceloserpa <jdev.marcelo@gmail.com>
2025-01-10 08:28:55 -0300

    maze: implement priority queue

--- chapter2/generic_search.py
+++ chapter2/generic_search.py
@@ -142,6 +142,25 @@ class Queue(Generic[T]):
         return repr(self._container)
 
 
+class PriorityQueue(Generic[T]):
+
+    def __init__(self) -> None:
+        self._container: List[T] = []
+
+    @property
+    def empty(self) -> bool:
+        return not self._container
+
+    def push(self, item: T) -> None:
+        heappush(self._container, item)
+
+    def pop(self) -> T:
+        return heappop(self._container)
+
+    def __repr__(self) -> str:
+        return repr(self._container)
+
+
 class Node(Generic[T]):
     def __init__(self, state: T, parent: Optional[Node], cost: float = 0.0, heuristic: float = 0.0) -> None:
         self.state: T = state
