5c6d2e50d7892a7bc7db4f159f61f9d8e2f04394
marceloserpa <jdev.marcelo@gmail.com>
2025-01-21 08:28:41 -0300

    implementing undirected graph

--- chapter4/graph.py
+++ chapter4/graph.py
@@ -8,5 +8,99 @@ V = TypeVar("V")
 class Graph(Generic[V]):
 
     def __init__(self, vertices: List[V] = []) -> None:
-        self._variables: List[V] = vertices
+        self._vertices: List[V] = vertices
         self._edges: List[List[Edge]] = [[] for _ in vertices]
+
+    @property
+    def vertex_count(self) -> int:
+        return len(self._vertices)
+
+    
+    @property
+    def edge_count(self) -> int:
+        return len(self._edges)
+
+    def add_vertex(self,vertex: V) -> int:
+        self._vertices.append(vertex)
+        self._edges.append([])
+        return self.vertex_count -1
+
+    # This is an undirectedgraph.
+    # always in both directions
+    def add_edge(self,edge: Edge) -> None:
+        self._edges[edge.u].append(edge)
+        self._edges[edge.v].append(edge.reversed())
+
+    # add edge using vertex indices
+    def add_by_indices(self,u: int, v:int) -> None:
+        edge: Edge = Edge(u, v)
+        self.add_edge(edge)
+
+    # ddan edge by looking up vertexindices
+    def add_edge_by_vertices(self,first: V, second: V) -> None:
+        u: int = self._vertices.index(first)
+        v: int = self._vertices.index(second)
+        self.add_by_indices(u,v)
+
+    def vertex_at(self,index: int) -> V:
+        return self._vertices[index]
+
+    def index_of(self, vertex: V) -> int:
+        return self._vertices.index(vertex)
+
+    # Find the vertices that a vertex at some index is connected to
+    def neighbors_for_index(self, index: int) -> List[V]:
+        return list(map(self.vertex_at, [e.v for e in self._edges[index]]))
+
+    # Look up a vertice's index and find its neighbors
+    def neighbors_for_vertex(self, vertex: V) -> List[V]:
+        return self.neighbors_for_index(self.index_of(vertex))
+
+    # Return all of the edges associated with a vertex at some index
+    def edges_for_index(self,index: int) -> List[Edge]:
+        return self._edges[index]
+    
+    # Look up the index of a vertex and returns its edges
+    def edges_for_vertex(self,vertex: V) -> List[V]:
+        return self.edges_for_index(self.index_of(vertex))
+
+    def __str__(self) -> str:
+        desc: str = ""
+        for i in range(self.vertex_count):
+            desc += f"{self.vertex_at(i)} -> {self.neighbors_for_index(i)} \n"
+        return desc
+
+
+if __name__ == "__main__":
+    # test basic Graph construction
+    city_graph: Graph[str] = Graph(["Seattle", "San Francisco", "Los Angeles", "Riverside", "Phoenix", "Chicago", "Boston", "New York",
+    "Atlanta", "Miami", "Dallas", "Houston", "Detroit", "Philadelphia", "Washington"])
+    
+    city_graph.add_edge_by_vertices("Seattle", "Chicago")
+    city_graph.add_edge_by_vertices("Seattle", "San Francisco")
+    city_graph.add_edge_by_vertices("San Francisco", "Riverside")
+    city_graph.add_edge_by_vertices("San Francisco", "Los Angeles")
+    city_graph.add_edge_by_vertices("Los Angeles", "Riverside")
+    city_graph.add_edge_by_vertices("Los Angeles", "Phoenix")
+    city_graph.add_edge_by_vertices("Riverside", "Phoenix")
+    city_graph.add_edge_by_vertices("Riverside", "Chicago")
+    city_graph.add_edge_by_vertices("Phoenix", "Dallas")
+    city_graph.add_edge_by_vertices("Phoenix", "Houston")
+    city_graph.add_edge_by_vertices("Dallas", "Chicago")
+    city_graph.add_edge_by_vertices("Dallas", "Atlanta")
+    city_graph.add_edge_by_vertices("Dallas", "Houston")
+    city_graph.add_edge_by_vertices("Houston", "Atlanta")
+    city_graph.add_edge_by_vertices("Houston", "Miami")
+    city_graph.add_edge_by_vertices("Atlanta", "Chicago")
+    city_graph.add_edge_by_vertices("Atlanta", "Washington")
+    city_graph.add_edge_by_vertices("Atlanta", "Miami")
+    city_graph.add_edge_by_vertices("Miami", "Washington")
+    city_graph.add_edge_by_vertices("Chicago", "Detroit")
+    city_graph.add_edge_by_vertices("Detroit", "Boston")
+    city_graph.add_edge_by_vertices("Detroit", "Washington")
+    city_graph.add_edge_by_vertices("Detroit", "New York")
+    city_graph.add_edge_by_vertices("Boston", "New York")
+    city_graph.add_edge_by_vertices("New York", "Philadelphia")
+    city_graph.add_edge_by_vertices("Philadelphia", "Washington")
+
+    print(city_graph)
